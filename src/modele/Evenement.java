b19bcf0cb4100ce8e0013f68f0125c48713adc2e
Heisenberg152 <guillaumebesson@laposte.net>
2016-08-07 15:40:27 +0200

    Ajout classe événement

---
+++ src/modele/Evenement.java
@@ -0,0 +1,15 @@
+package modele;
+
+import java.util.List;
+
+import modele.Depense;
+import modele.Personne;
+
+public class Evenement {
+
+	private int idEvenement;
+	private String libelleEvenement;
+	private List<Depense> listeDepense;
+	private List<Personne> listeParticipants;
+	
+}
